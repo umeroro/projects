dcb8e7a02a3a735f71ed3da217cfee356a27a752
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2024-07-23 23:05:36 +0500

    Todo Tracker

--- javascript/Todo Tracker/file.js
+++ javascript/Todo Tracker/file.js
@@ -2,7 +2,14 @@ console.log('Server is running on port 3000');
 var todoHeader = document.getElementById('title-header');
 todoHeader.textContent = 'My Todo Tracker';
 document.querySelector('li').textContent = 'Wake up';
-var todo = document.getElementsByClassName('todos')[0];
-todo.textContent = 'Sleep';
+var input = document.getElementById('user-todos');
+// input.textContent = 'Sleep';
 var list = document.querySelector('ul');
-list.appendChild(todo);
+// list.appendChild(input);
+document.getElementById("clerAll").addEventListener('click', function(){
+    console.log("You have clicked the button")
+})
+document.querySelector('form').addEventListener('submit', function(e){
+    e.preventDefault();
+    console.log(input.value);
+})
