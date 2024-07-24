b554513c04b24176f32d56ddb583782c7d80230a
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2024-07-24 23:39:30 +0500

    now it able to add the task and clear the task

--- javascript/Todo Tracker/file.js
+++ javascript/Todo Tracker/file.js
@@ -1,15 +1,22 @@
-console.log('Server is running on port 3000');
-var todoHeader = document.getElementById('title-header');
-todoHeader.textContent = 'My Todo Tracker';
-document.querySelector('li').textContent = 'Wake up';
-var input = document.getElementById('user-todos');
-// input.textContent = 'Sleep';
-var list = document.querySelector('ul');
-// list.appendChild(input);
-document.getElementById("clerAll").addEventListener('click', function(){
-    console.log("You have clicked the button")
-})
-document.querySelector('form').addEventListener('submit', function(e){
+var form = document.querySelector('form');
+var todosList = document.querySelector('ul');
+var button = document.getElementById('clerAll');
+var input = document.getElementById('taskInput');
+
+form.addEventListener('submit', function(e){
     e.preventDefault();
-    console.log(input.value);
+    todoMaker(input.value);
+    input.value = '';
+})
+
+var todoMaker = function(task){
+    var todo = document.createElement('li');
+    todo.textContent = task;
+    todosList.appendChild(todo);
+}
+
+button.addEventListener('click', function(){
+    while(todosList.firstChild){
+        todosList.removeChild(todosList.firstChild);
+    }
 })
