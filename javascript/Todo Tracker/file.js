5aba47464287eb4fbccb7a72ba8d37d0a05231ef
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2024-07-26 23:24:44 +0500

    Todo Tracker completed

--- javascript/Todo Tracker/file.js
+++ javascript/Todo Tracker/file.js
@@ -2,9 +2,16 @@ var form = document.querySelector('form');
 var todosList = document.querySelector('ul');
 var button = document.getElementById('clerAll');
 var input = document.getElementById('taskInput');
+// var todosArray = [];
+var todosArray = localStorage.getItem('task') ? JSON.parse(localStorage.getItem('task')) : [];
+
+localStorage.setItem('task', JSON.stringify(todosArray));
+var storage = JSON.parse(localStorage.getItem('task'));
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
+    todosArray.push(input.value);
+    localStorage.setItem('task', JSON.stringify(todosArray));
     todoMaker(input.value);
     input.value = '';
 })
@@ -15,7 +22,12 @@ var todoMaker = function(task){
     todosList.appendChild(todo);
 }
 
+for(var i=0; i< storage.length; i++){
+    todoMaker(storage[i]);
+}
+
 button.addEventListener('click', function(){
+    localStorage.clear();
     while(todosList.firstChild){
         todosList.removeChild(todosList.firstChild);
     }
