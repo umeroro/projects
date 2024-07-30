a736be2b436cd837f58bed70a85074342c3b9b60
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2024-07-30 23:42:21 +0500

    duplicate the cards

--- javascript/Memory Based card game/main.js
+++ javascript/Memory Based card game/main.js
@@ -1,13 +1,23 @@
-var cardsarray = [
-    { 'name': 'color-pencil' , 'img': 'https://images.pexels.com/photos/230514/pexels-photo-230514.jpeg?auto=compress&cs=tinysrgb&w=600'},
-    { 'name': 'globe' , 'img': 'https://images.pexels.com/photos/1236421/pexels-photo-1236421.jpeg?auto=compress&cs=tinysrgb&w=600'},
-    { 'name': 'books' , 'img': 'https://images.pexels.com/photos/4690297/pexels-photo-4690297.jpeg?auto=compress&cs=tinysrgb&w=600'},
-    { 'name': 'chess' , 'img': 'https://images.pexels.com/photos/4973821/pexels-photo-4973821.jpeg?auto=compress&cs=tinysrgb&w=600'},
-    { 'name': 'glitter' , 'img': 'https://images.pexels.com/photos/4690307/pexels-photo-4690307.jpeg?auto=compress&cs=tinysrgb&w=600'},
-    { 'name': 'computer' , 'img': 'https://images.pexels.com/photos/5082556/pexels-photo-5082556.jpeg?auto=compress&cs=tinysrgb&w=600'}
+var cardsArray = [
+    { 'name': 'color-pencil' , 'img': 'https://images.pexels.com/photos/230514/pexels-photo-230514.jpeg?auto=compress&cs=tinysrgb&w=600',},
+    { 'name': 'globe' , 'img': 'https://images.pexels.com/photos/1236421/pexels-photo-1236421.jpeg?auto=compress&cs=tinysrgb&w=600',},
+    { 'name': 'books' , 'img': 'https://images.pexels.com/photos/4690297/pexels-photo-4690297.jpeg?auto=compress&cs=tinysrgb&w=600',},
+    { 'name': 'chess' , 'img': 'https://images.pexels.com/photos/4973821/pexels-photo-4973821.jpeg?auto=compress&cs=tinysrgb&w=600',},
+    { 'name': 'glitter' , 'img': 'https://images.pexels.com/photos/4690307/pexels-photo-4690307.jpeg?auto=compress&cs=tinysrgb&w=600',},
+    { 'name': 'computer' , 'img': 'https://images.pexels.com/photos/5082556/pexels-photo-5082556.jpeg?auto=compress&cs=tinysrgb&w=600',}
 ];
+
+var gameGrid = cardsArray.concat(cardsArray);
 
 var game = document.getElementById('game-board');
 var grid = document.createElement('section');
 grid.setAttribute('class', 'grid')
 game.appendChild(grid);
+
+for (i=0; i < gameGrid.length; i++){
+    var card = document.createElement('div');
+    card.classList.add('card');
+    card.dataset.name = gameGrid[i].name;
+    card.style.backgroundImage = `url(${gameGrid[i].img})`;
+    grid.appendChild(card);
+}
