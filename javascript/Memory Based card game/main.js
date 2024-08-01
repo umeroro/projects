10011d16e472de3b039e0ef2b043235dbbe2f87f
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2024-08-01 22:45:40 +0500

    the border of the image is shown when we clicked on

--- javascript/Memory Based card game/main.js
+++ javascript/Memory Based card game/main.js
@@ -1,35 +1,42 @@
 var cardsArray = [
-    {    'name': 'CSS',    'img': 'https://github.com/robgmerrill/img/blob/master/css3-logo.png?raw=true',  },
-    {    'name': 'HTML',    'img': 'https://github.com/robgmerrill/img/blob/master/html5-logo.png?raw=true',  },
-    {    'name': 'jQuery',    'img': 'https://github.com/robgmerrill/img/blob/master/jquery-logo.png?raw=true',  },
-    {    'name': 'JS',    'img': 'https://github.com/robgmerrill/img/blob/master/js-logo.png?raw=true',  },
-    {    'name': 'Node',    'img': 'https://github.com/robgmerrill/img/blob/master/nodejs-logo.png?raw=true',  },
-    {    'name': 'Photo Shop',    'img': 'https://github.com/robgmerrill/img/blob/master/photoshop-logo.png?raw=true',  },
-    {    'name': 'PHP',    'img': 'https://github.com/robgmerrill/img/blob/master/php-logo_1.png?raw=true',  },
-    {    'name': 'Python',    'img': 'https://github.com/robgmerrill/img/blob/master/python-logo.png?raw=true',  },
-    {    'name': 'Ruby',    'img': 'https://github.com/robgmerrill/img/blob/master/rails-logo.png?raw=true',  },
-    {    'name': 'Sass',    'img': 'https://github.com/robgmerrill/img/blob/master/sass-logo.png?raw=true',  },
-    {    'name': 'Sublime',    'img': 'https://github.com/robgmerrill/img/blob/master/sublime-logo.png?raw=true',  },
-    {    'name': 'Wordpress',    'img': 'https://github.com/robgmerrill/img/blob/master/wordpress-logo.png?raw=true',  },
-  ];
-  
-  var gameGrid = cardsArray.concat(cardsArray);
-  
-  gameGrid.sort(function() {
-    return 0.5 - Math.random();
-  })
-  
-  var game = document.getElementById('game-board');
-  var grid = document.createElement('section');
-  
-  grid.setAttribute('class', 'grid');
-  game.appendChild(grid);
-  
-  
-  for (i = 0; i < gameGrid.length; i++) {
-    var card = document.createElement('div');
-    card.classList.add('card');
-    card.dataset.name = gameGrid[i].name;
-    card.style.backgroundImage = `url(${gameGrid[i].img})`;
-    grid.appendChild(card);
+  { 'name': 'CSS', 'img': 'https://github.com/robgmerrill/img/blob/master/css3-logo.png?raw=true' },
+  { 'name': 'HTML', 'img': 'https://github.com/robgmerrill/img/blob/master/html5-logo.png?raw=true' },
+  { 'name': 'jQuery', 'img': 'https://github.com/robgmerrill/img/blob/master/jquery-logo.png?raw=true' },
+  { 'name': 'JS', 'img': 'https://github.com/robgmerrill/img/blob/master/js-logo.png?raw=true' },
+  { 'name': 'Node', 'img': 'https://github.com/robgmerrill/img/blob/master/nodejs-logo.png?raw=true' },
+  { 'name': 'Photo Shop', 'img': 'https://github.com/robgmerrill/img/blob/master/photoshop-logo.png?raw=true' },
+  { 'name': 'PHP', 'img': 'https://github.com/robgmerrill/img/blob/master/php-logo_1.png?raw=true' },
+  { 'name': 'Python', 'img': 'https://github.com/robgmerrill/img/blob/master/python-logo.png?raw=true' },
+  { 'name': 'Ruby', 'img': 'https://github.com/robgmerrill/img/blob/master/rails-logo.png?raw=true' },
+  { 'name': 'Sass', 'img': 'https://github.com/robgmerrill/img/blob/master/sass-logo.png?raw=true' },
+  { 'name': 'Sublime', 'img': 'https://github.com/robgmerrill/img/blob/master/sublime-logo.png?raw=true' },
+  { 'name': 'Wordpress', 'img': 'https://github.com/robgmerrill/img/blob/master/wordpress-logo.png?raw=true' },
+];
+
+var gameGrid = cardsArray.concat(cardsArray);
+
+gameGrid.sort(function() {
+  return 0.5 - Math.random();
+});
+
+var game = document.getElementById('game-board');
+var grid = document.createElement('section');
+
+grid.setAttribute('class', 'grid');
+game.appendChild(grid);
+
+for (var i = 0; i < gameGrid.length; i++) {
+  var card = document.createElement('div');
+  card.classList.add('card');
+  card.dataset.name = gameGrid[i].name;
+  card.style.backgroundImage = `url(${gameGrid[i].img})`;
+  grid.appendChild(card);
+}
+
+grid.addEventListener('click', function(e){
+  var clicked = e.target;
+  if(clicked.nodeName === 'SECTION'){
+    return;
   }
+  clicked.classList.add('selected');
+})
